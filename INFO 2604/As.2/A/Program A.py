428ab1d875b45ae5be73aa4156899d317fd715b4
ParaDucks <akilhosang@gmail.com>
2018-03-20 02:09:02 -0400

    Update Files

--- INFO 2604/As.2/A/Program A.py
+++ INFO 2604/As.2/A/Program A.py
@@ -3,10 +3,64 @@ import Crypto
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import DES
 
+def getSharedKey():
+    p = 13
+    q = 23
+
+    progASecret = 17
+
+    A = (p**progASecret)%q
+
+    print "Sending..."
+    s.send(str(A))
+
+    B = int(s.recv(1024))
+    print "Done Recieving"
+    sharedSecretKey = (B**progASecret)%q
+
+    return A, B, sharedSecretKey
+
+def encCeasarCipher(msg, key):
+    LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+    ULETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    translated = ''
+    
+    for symbol in msg:
+        if symbol.isalpha():
+            if symbol in LETTERS:
+                num = LETTERS.find(symbol)
+                num += key
+
+                if num >= len (LETTERS):
+                    num -= len(LETTERS)
+                elif num < 0:
+                    num += len(LETTERS)
+
+                translated += LETTERS[num]
+                
+            else:
+                num = ULETTERS.find(symbol)
+                num += key
+
+                if num >= len (ULETTERS):
+                    num -= len(ULETTERS)
+                elif num < 0:
+                    num += len(ULETTERS)
+
+                translated += ULETTERS[num]
+        else:
+            translated += symbol
+
+    return translated
+
 s = socket.socket()
 host = socket.gethostname()
-port = 4422
+port = 1717
 s.connect((host, port))
+
+f = open("NoWar.dat", 'rb')
+msg = f.read(1024)
+f.close()
 
 rsaKey = RSA.generate(1024)
 
@@ -21,30 +75,42 @@ des = DES.new(key)
 
 rsaPrivateKey += ' ' * (8 - len(rsaPrivateKey) % 8) # Pad the key if needed
 
-encryptedRSAPrivateKey = des.encrypt(rsaPrivateKey)
+encRSAPrivateKey = des.encrypt(rsaPrivateKey)
 
-privateKey = des.decrypt(encryptedRSAPrivateKey).strip()
+#privateKey = des.decrypt(encryptedRSAPrivateKey).strip()
 
+encRSAMsg = rsaPublicObj.encrypt(msg, "random")[0]
 
 f = open("privatekey.dat", 'wb')
-f.write(encryptedRSAPrivateKey)
+f.write(encRSAPrivateKey)
 f.close()
 
 f = open("publickey.dat", 'wb')
 f.write(rsaPublicKey)
 f.close()
 
-f = open("NoWar.dat", 'rb')
-msg = f.read(1024)
-f.close()
-
 f = open("secrets.dat", 'wb')
-encRSAmsg = rsaPublicObj.encrypt(msg, "random")[0]
-f.write(encRSAmsg)
+f.write(encRSAMsg)
 f.close()
 
 print "Sending..."
-print encRSAmsg
-s.send(encRSAmsg)
+s.send(encRSAMsg)
 
 print "Done Sending"
+
+A, B, sharedSecretKey = getSharedKey()
+
+ceasarEncRSAMsg = encCeasarCipher(encRSAMsg, sharedSecretKey)
+ceasarEncDESKey = encCeasarCipher(key, sharedSecretKey)
+ceasarEncPrivateKey = encCeasarCipher(encRSAPrivateKey, sharedSecretKey)
+
+s.send(bytes(ceasarEncRSAMsg))
+print s.recv(1024)
+s.send(bytes(ceasarEncDESKey))
+print s.recv(1024)
+s.send(bytes(ceasarEncPrivateKey))
+print s.recv(1024)
+
+print "\n\nTHE RSA MSG IS \n", encRSAMsg
+print "\nTHE DES KEY IS \n", key
+print "\nTHE PRIVATE KEY IS \n", encRSAPrivateKey
--- INFO 2604/As.2/B/B.py
+++ INFO 2604/As.2/B/B.py
@@ -1,7 +1,60 @@
 import socket
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import DES
+
+def getSharedKey():
+    p = 13
+    q = 23
+    progBSecret = 38
+    B = (p**progBSecret)%q
+
+    A = int(c.recv(1024))
+
+    sharedSecretKey = (A**progBSecret)%q
+
+    c.send(str(B))
+    print("Done Recieving")
+
+    return A, B, sharedSecretKey
+
+def decCeasarCipher(msg, key):
+    LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+    ULETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    translated = ''
+    
+    for symbol in msg:
+        if symbol.isalpha():
+            if symbol in LETTERS:
+                num = LETTERS.find(symbol)
+                num -= key
+
+                if num >= len (LETTERS):
+                    num -= len(LETTERS)
+                elif num < 0:
+                    num += len(LETTERS)
+
+                translated += LETTERS[num]
+                
+            else:
+                num = ULETTERS.find(symbol)
+                
+                num -= key
+
+                if num >= len (ULETTERS):
+                    num -= len(ULETTERS)
+                elif num < 0:
+                    num += len(ULETTERS)
+
+                translated += ULETTERS[num]
+        else:
+            translated += symbol
+
+    return translated
+
 s = socket.socket()
 host = socket.gethostname()
-port = 4422
+port = 1717
 s.bind(("", port))
 
 f = open('secrets.dat', 'wb')
@@ -9,19 +62,50 @@ s.listen(1)
 
 print "Waiting for connection..."
 
-while True:
-    c, addr = s.accept()
-    print "Connection made from", addr
-    print "Receiving..."
+c, addr = s.accept()
+print "Connection made from", addr
+print "Receiving..."
 
-    msg = c.recv(1024)
-    f.write(msg)
+encRSAMsg = c.recv(1024)
+f.write(encRSAMsg)
 
-    f = open('secrets.dat', 'rb')
-    msg = f.read(1024)
+f = open('secrets.dat', 'rb')
+encRSAMsg = f.read(1024)
 
-    f.close()
-    break
+f.close()
 
-print msg
-print("Done Recieving")
+A, B, sharedSecretKey = getSharedKey()
+
+print "Recieving..."
+
+ceasarEncRSAMsg = c.recv(1024)
+c.send("Ceasar Encrypted RSA Message Recieved")
+ceasarEncDESKey = c.recv(1024)
+c.send("Ceasar Encrypted DES Key Recieved")
+ceasarEncPrivateKey = c.recv(1024)
+c.send("Ceasar Encrypted RSA Message Recieved")
+
+print "Done Recieving"
+c.close()
+
+
+encRSAMsg = decCeasarCipher(ceasarEncRSAMsg, sharedSecretKey)
+key = decCeasarCipher(ceasarEncDESKey, sharedSecretKey)
+encRSAPrivateKey = decCeasarCipher(ceasarEncPrivateKey, sharedSecretKey)
+
+print "\n\nTHE RSA MSG IS \n", encRSAMsg
+print "\nTHE DES KEY IS \n", key
+print "\nTHE PRIVATE KEY IS \n", encRSAPrivateKey
+
+des = DES.new(key)
+
+privateKey = des.decrypt(encRSAPrivateKey)
+
+print privateKey
+
+privateRSAKey = RSA.importKey(privateKey)
+
+decrypted = privateRSAKey.decrypt(encRSAMsg)
+
+print decrypted
+
